1802d5db12eb4a9dfc08f6f9e015c213b681f8df
amoohashem <amoo.hashem.mehraban@gmail.com>
2021-08-16 03:26:12 +0430

    fix a bug + remove redundant route

--- src/root/Root.dev.js
+++ src/root/Root.dev.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import MiniGames from '../components/MiniGames';
-import SummerSchoolLanding from '../containers/SummerSchool';
 import AboutUs from '../containers/AboutUs';
 import OurTeam from '../containers/OurTeam';
+import SummerSchoolLanding from '../containers/SummerSchool';
 
 
 const Root = () => {
   return (
     <Switch>
-      <Route path="/game/:gameId" component={MiniGames} />
-      <Route path="/" component={SummerSchoolLanding} />
       <Route path="/about_us" component={AboutUs} />
       <Route path="/our_team" component={OurTeam} />
+      <Route path="/" component={SummerSchoolLanding} />
     </Switch>
   );
 };
