994942da3787d787361d910b07bf3812c74bfe79
amoohashem <amoo.hashem.mehraban@gmail.com>
2021-08-15 05:49:45 +0430

    remove some redundant imports

--- src/root/Root.prod.js
+++ src/root/Root.prod.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import MiniGames from '../components/MiniGames';
-import Article from '../containers/Article';
-import PhysicsDayLanding from '../containers/PhysicsDay';
+import SummerSchoolLanding from '../containers/SummerSchool';
 
 const Root = () => {
   return (
     <Switch>
       <Route path="/game/:gameId" component={MiniGames} />
-      <Route path="/article/:articleId" component={Article} />
-      <Route path="/" component={PhysicsDayLanding} />
+      <Route path="/" component={SummerSchoolLanding} />
     </Switch>
   );
 };
