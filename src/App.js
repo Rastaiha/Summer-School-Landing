5765be53dfdfb20ae99e923ad29115ec9328b1ca
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2023-07-29 20:24:54 +0330

    make absoulte import

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ import { CssBaseline, LinearProgress } from '@mui/material';
 import { ThemeProvider } from '@mui/material';
 import { CacheProvider } from "@emotion/react";
 import { useNavigate } from 'react-router-dom';
-import CreateEmotionCache from './configs/CreateEmotionCache'
-import SelectTheme from './configs/themes';
-import Root from './routes';
-
+import CreateEmotionCache from 'configs/CreateEmotionCache'
+import SelectTheme from 'configs/themes';
+import Root from 'routes';
 
 const App = ({ dir = 'rtl' }) => {
   const navigate = useNavigate();
